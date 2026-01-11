63afb4c8099b1a837cbf39afb6eaf026919fa00d
Hani Manoj <hanimanoj@gmail.com>
2026-01-11 20:59:48 +0800

    footer update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,32 +1,30 @@
 <footer class="footer">
     <div class="footer-top-row">
-        <div class="footer-contact">
+
+        {{-- CONTACT --}}
+        <div class="footer-section">
             <h4>CONTACT US</h4>
             <div class="footer-icons">
-                <img src="{{ asset('email.png') }}" alt="email" class="email-icon">
-                <img src="{{ asset('call.png') }}" alt="call" class="call-icon">
+                <img src="{{ asset('images/logos/email.png') }}" alt="email">
+                <img src="{{ asset('images/logos/call.png') }}" alt="call">
             </div>
         </div>
 
-        <div class="footer-follow">
+        {{-- FOLLOW --}}
+        <div class="footer-section">
             <h4>FOLLOW US</h4>
             <div class="footer-icons">
-                <img src="{{ asset('ig.png') }}" alt="ig" class="ig-icon">
-                <img src="{{ asset('tiktok.png') }}" alt="tiktok" class="tiktok-icon">
+                <img src="{{ asset('images/logos/ig.png') }}" alt="ig">
+                <img src="{{ asset('images/logos/tiktok.png') }}" alt="tiktok">
             </div>
         </div>
+
     </div>
 
-        <hr>
-    <div class="footer-bottom-row">
-        <p class = "faq">
-            Help | FAQs | Terms of Use & Privacy Policy
-        </p>
-        
-        <p class="footer-copy">
-            © 2026 Halal Bites Gombak. All Rights Reserved.
-        </p>
+    <hr class="footer-divider">
 
+    <div class="footer-bottom-row">
+        <p class="faq">Help | FAQs | Terms of Use & Privacy Policy</p>
+        <p class="footer-copy">© 2025 Halal Bites Gombak. All Rights Reserved.</p>
     </div>
 </footer>
-
