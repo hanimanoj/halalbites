aad584999a49de996408b36778727758800a54ee
imanadrienna <151700356+imanadrienna@users.noreply.github.com>
2026-01-12 20:17:42 +0800

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,8 +5,12 @@
         <div class="footer-section">
             <h4>CONTACT US</h4>
             <div class="footer-icons">
-                <img src="{{ asset('images/logos/email.png') }}" alt="email">
-                <img src="{{ asset('images/logos/call.png') }}" alt="call">
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=halalbitesgombak@gmail.com" target="_blank">
+                    <img src="{{ asset('email.png') }}" alt="email" class="email-icon">
+                </a>
+                <a href="tel:+60195788809">
+                <img src="{{ asset('call.png') }}" alt="call" class="call-icon">
+                </a>
             </div>
         </div>
 
@@ -14,8 +18,12 @@
         <div class="footer-section">
             <h4>FOLLOW US</h4>
             <div class="footer-icons">
-                <img src="{{ asset('images/logos/ig.png') }}" alt="ig">
-                <img src="{{ asset('images/logos/tiktok.png') }}" alt="tiktok">
+                <a href="https://www.instagram.com/halalbitesgombak?utm_source=qr&igsh=cjY4dHJzdjduamVi" target="_blank">
+                    <img src="{{ asset('ig.png') }}" alt="ig" class="ig-icon">
+                </a>
+                <a href="https://www.tiktok.com/@halalbitesgombak?_r=1&_d=eld1dfc7d674lm&sec_uid=MS4wLjABAAAAXz2UThQaioaP7hsVYCw1kRcUtxDET3RAMGw1zptnvXq5Mpjmj_jglzXT2tr2BeMH&share_author_id=7594088675039609864&sharer_language=en&source=h5_t&u_code=f1c4g4jdi7m37d&timestamp=1768136902&user_id=7594088675039609864&sec_user_id=MS4wLjABAAAAXz2UThQaioaP7hsVYCw1kRcUtxDET3RAMGw1zptnvXq5Mpjmj_jglzXT2tr2BeMH&item_author_type=1&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7547216730688194322&share_link_id=80f9ad2c-f2a6-4680-bc7b-2f7b8b2a84ed&share_app_id=1180&ugbiz_name=ACCOUNT&ug_btm=b8727%2Cb7360&social_share_type=5&enable_checksum=1" target="_blank">
+                    <img src="{{ asset('tiktok.png') }}" alt="tiktok" class="tiktok-icon">
+                </a>
             </div>
         </div>
 
